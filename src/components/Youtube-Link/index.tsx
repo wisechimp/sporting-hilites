33fccb2b6c7cc4fb76d0f361e2aa6fe7aac7e69a
Stuart Hicks <stuart.hicks@gmail.com>
2026-01-23 17:40:07 +0100

    content: PL 2025 MW 23

--- src/components/Youtube-Link/index.tsx
+++ src/components/Youtube-Link/index.tsx
@@ -1,11 +1,15 @@
 import { FaYoutube } from "react-icons/fa";
 
 type YoutubeLinkProps = {
+  // Change prop's name to video code or similar
   url: string;
 };
 
+// Create a Youtube Linkbuilder function that will create a link from the video's code
+
 const YoutubeLink = ({ url }: YoutubeLinkProps) => {
   return (
+    // url should become Youtube link created in the fuction
     <a href={url} target="_blank" rel="noopener noreferrer">
       <FaYoutube color="red" />
     </a>
