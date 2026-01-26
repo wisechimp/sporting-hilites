26d7aec1457a3a4b12f73db7f4234abbda874884
Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
2026-01-26 15:05:52 +0100

    feat: Added Youtube Link Builder function
    Takes the video's code and builds a youtube link

--- src/components/Youtube-Link/index.tsx
+++ src/components/Youtube-Link/index.tsx
@@ -1,15 +1,16 @@
 import { FaYoutube } from "react-icons/fa";
 
 type YoutubeLinkProps = {
-  // Change prop's name to video code or similar
-  url: string;
+  videoCode: string;
 };
 
-// Create a Youtube Linkbuilder function that will create a link from the video's code
+const youtubeLinkBuilder = (videoCode: string) => {
+  return `https://www.youtube.com/watch?v=${videoCode}`;
+};
 
-const YoutubeLink = ({ url }: YoutubeLinkProps) => {
+const YoutubeLink = ({ videoCode }: YoutubeLinkProps) => {
+  const url = youtubeLinkBuilder(videoCode);
   return (
-    // url should become Youtube link created in the fuction
     <a href={url} target="_blank" rel="noopener noreferrer">
       <FaYoutube color="red" />
     </a>
